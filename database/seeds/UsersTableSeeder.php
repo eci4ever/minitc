2d5b1fdf683c7d060506c628f30c982f1d4de27f
eci4ever <eci4ever@gmail.com>
2020-04-29 12:52:19 +0800

    modify seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -8,36 +8,37 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $users = [
-        [
-            'id'             => 1,
-            'name'           => 'Admin',
-            'email'          => 'admin@admin.com',
-            'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
-            'remember_token' => null,
-            'created_at'     => '2019-04-15 19:13:32',
-            'updated_at'     => '2019-04-15 19:13:32',
-            'deleted_at'     => null,
-        ],
-        [
-            'id'             => 2,
-            'name'           => 'Manager',
-            'email'          => 'manager@local',
-            'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
-            'remember_token' => null,
-            'created_at'     => '2019-04-15 19:13:32',
-            'updated_at'     => '2019-04-15 19:13:32',
-            'deleted_at'     => null,
-        ],
-        [
-            'id'             => 3,
-            'name'           => 'User',
-            'email'          => 'user@local',
-            'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
-            'remember_token' => null,
-            'created_at'     => '2019-04-15 19:13:32',
-            'updated_at'     => '2019-04-15 19:13:32',
-            'deleted_at'     => null,
-        ]];
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@local.com',
+                'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
+                'remember_token' => null,
+                'created_at'     => '2020-04-15 19:13:32',
+                'updated_at'     => '2020-04-15 19:13:32',
+                'deleted_at'     => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'Manager',
+                'email'          => 'manager@local.com',
+                'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
+                'remember_token' => null,
+                'created_at'     => '2020-04-15 19:13:32',
+                'updated_at'     => '2020-04-15 19:13:32',
+                'deleted_at'     => null,
+            ],
+            [
+                'id'             => 3,
+                'name'           => 'User',
+                'email'          => 'user@local.com',
+                'password'       => '$2y$10$imU.Hdz7VauIT3LIMCMbsOXvaaTQg6luVqkhfkBcsUd.SJW2XSRKO',
+                'remember_token' => null,
+                'created_at'     => '2020-04-15 19:13:32',
+                'updated_at'     => '2020-04-15 19:13:32',
+                'deleted_at'     => null,
+            ]
+        ];
 
         User::insert($users);
     }
